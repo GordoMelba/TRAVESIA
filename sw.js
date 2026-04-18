51866369387452a108d4a87aa47ca9974820034e
BotoneraEnri <160452621+GordoMelba@users.noreply.github.com>
2026-04-18 13:07:01 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'travesias-v4';
+const FILES = [
+  '/TRAVESIA/',
+  '/TRAVESIA/index.html',
+  '/TRAVESIA/travesias.html',
+  '/TRAVESIA/manifest.json'
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache){
+      return cache.addAll(FILES).catch(function(err){
+        console.log('Cache partial:', err);
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){return k!==CACHE;})
+          .map(function(k){return caches.delete(k);})
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    fetch(e.request).then(function(response){
+      const clone = response.clone();
+      caches.open(CACHE).then(function(cache){cache.put(e.request, clone);});
+      return response;
+    }).catch(function(){
+      return caches.match(e.request).then(function(cached){
+        return cached || caches.match('/TRAVESIA/travesias.html');
+      });
+    })
+  );
+});
