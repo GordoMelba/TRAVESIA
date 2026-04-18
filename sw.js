48a2c4edb937b9775253988991158c09ba00e1ea
BotoneraEnri <160452621+GordoMelba@users.noreply.github.com>
2026-04-18 13:16:56 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+// Service worker minimo - limpia caches viejos y no cachea nada
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+// No intercepta fetch - deja pasar todo al servidor
